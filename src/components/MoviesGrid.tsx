c570764e5270be906b120e452e61d41196330688
nastassia martin <nastassia.martin@googlemail.com>
2023-08-28 10:00:19 +0200

    created component of Movie Grid, installed font-awesome package, improved css for Movie Grid

---
+++ src/components/MoviesGrid.tsx
@@ -0,0 +1,61 @@
+import { Link } from "react-router-dom"
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faStar } from "@fortawesome/free-solid-svg-icons"
+
+import fallback from "../assets/images/popcorn.jpg"
+import Container from "react-bootstrap/Container"
+import Card from "react-bootstrap/Card"
+import { GenericMovieResponse } from "../types/index.types"
+import React from "react"
+
+interface IProps {
+	data: GenericMovieResponse
+}
+const MoviesGrid: React.FC<IProps> = ({ data }) => {
+	const imageURL = "https://image.tmdb.org/t/p/w300"
+	return (
+		<>
+			<h2 className='container'>Popular Movies out now</h2>
+			<Container className='movie-container'>
+				{data.results.map((result) => (
+					<Card
+						as={Link}
+						to={`/movies/${result.id}`}
+						key={result.id}
+						className='movies'
+					>
+						{result.backdrop_path === null ? (
+							<Card.Img
+								className='movie-fallback-img'
+								src={fallback}
+								alt={result.title}
+							/>
+						) : (
+							<Card.Img
+								variant='top'
+								src={imageURL + result.backdrop_path}
+								alt={result.title}
+							/>
+						)}
+						<Card.Body className='movie-card-body-container'>
+							<Card.Title className='movie-card-body-title'>
+								{result.title}
+							</Card.Title>
+							<span className='class-card-body-rating'>
+								Communnity rating:&nbsp;&nbsp;{result.vote_average}
+								&nbsp;
+							</span>
+							<span>
+								<FontAwesomeIcon icon={faStar} style={{ color: "#ff40ff" }} />
+							</span>
+							<Card.Text className='text-truncate'>{result.overview}</Card.Text>
+						</Card.Body>
+					</Card>
+				))}
+			</Container>
+		</>
+	)
+}
+
+export default MoviesGrid
