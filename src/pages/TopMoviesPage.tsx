ae73662451a8e833dcde54e5a427e115f8d9f5fc
nastassia martin <nastassia.martin@googlemail.com>
2023-08-28 10:15:16 +0200

    removed dupplicate code from TopMoviesPage and added MovieGrid

--- src/pages/TopMoviesPage.tsx
+++ src/pages/TopMoviesPage.tsx
@@ -1,17 +1,10 @@
 import { useQuery } from "@tanstack/react-query"
-import { Link } from "react-router-dom"
-
 import Alert from "react-bootstrap/Alert"
-import Card from "react-bootstrap/Card"
-import Container from "react-bootstrap/Container"
-import Row from "react-bootstrap/Row"
-import fallback from "../assets/images/popcorn.jpg"
 
 import { getTopListedMovies } from "../services/themoviedbAPI"
+import MoviesGrid from "../components/MoviesGrid"
 
 const TopMoviesPage = () => {
-	const imageURL = "https://image.tmdb.org/t/p"
-	const width = "/w342"
 	const { data, isError } = useQuery({
 		queryKey: ["movies", "top-listed-movies"],
 		queryFn: getTopListedMovies,
@@ -23,36 +16,7 @@ const TopMoviesPage = () => {
 				<Alert variant='danger'>Oh no, something bad happened?</Alert>
 			)}
 
-			<Container className='p-4'>
-				<Row className='g-4'>
-					{data &&
-						data.results.map((res) => (
-							<Card
-								as={Link}
-								to={`/movies/${res.id}`}
-								key={res.id}
-								className='movies'
-							>
-								{res.backdrop_path === null ? (
-									<Card.Img className='fluid' src={fallback} alt={res.title} />
-								) : (
-									<Card.Img
-										className='fluid'
-										src={imageURL + width + res.backdrop_path}
-										alt={res.title}
-									/>
-								)}
-								<Card.ImgOverlay>
-									<Card.Title>{res.title}</Card.Title>
-									<Card.Text>
-										vote average: {res.vote_average}, total votes:
-										{res.vote_count}
-									</Card.Text>
-								</Card.ImgOverlay>
-							</Card>
-						))}
-				</Row>
-			</Container>
+			{data && <MoviesGrid title={"Top Movies"} data={data} />}
 		</>
 	)
 }
