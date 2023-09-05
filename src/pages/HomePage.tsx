8db86b893a819651aba796d510f9b16d323f5e53
nastassia martin <nastassia.martin@googlemail.com>
2023-09-05 21:35:10 +0200

    can now  movie from local storage and render to Home Page 🔥

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,6 +7,8 @@ import useTrending from "../hooks/useTrending"
 import MoviesGrid from "../components/MoviesGrid"
 import LoadingSpinner from "../components/LoadingSpinner"
 import ErrorAlert from "../components/ErrorAlert"
+import useLocalStorage from "../hooks/useLocalStorage"
+import { GenericMovieData } from "../types/movies.types"
 
 const HomePage = () => {
 	//use searchParams obj to read key/value `timeWindow: 'week'`
@@ -19,6 +21,22 @@ const HomePage = () => {
 
 	const { data, error, isLoading } = useTrending(timeWindow)
 
+	// const [localStorageMovies, setLocalStorageMovies] = useState<
+	// 	GenericMovieData[]
+	// >(window.localStorage.getItem("movie"))
+	// if (localStorageMovies) {
+	// 	localStorageMovies.map((movie) => {
+	// 		console.log(movie.title)
+	// 	})
+	// }
+
+	const [storedValue] = useLocalStorage<GenericMovieData[]>("movie", [])
+	if (storedValue) {
+		storedValue.map((movie) => {
+			console.log(movie.title)
+		})
+	}
+	//console.log(storedValue)
 	return (
 		<>
 			{error && (
@@ -32,11 +50,6 @@ const HomePage = () => {
 						<Card.Body>
 							<Card.Title>Genres</Card.Title>
 							<Card.Text>Deep dive into your favourite genres</Card.Text>
-							{/* <Link to={"/genres"}>
-									<Button className='pink' variant='dark'>
-										Genres
-									</Button>
-								</Link> */}
 						</Card.Body>
 					</Card>
 					<Card className='home-card-anchor' as={Link} to={"/movies/popular"}>
@@ -45,22 +58,12 @@ const HomePage = () => {
 							<Card.Text>
 								Browse the most popular movies out right now!
 							</Card.Text>
-							{/* <Link to={"/movies/popular"}>
-										<Button className='pink' variant='dark'>
-											Popular movies
-										</Button>
-									</Link> */}
 						</Card.Body>
 					</Card>
 					<Card className='home-card-anchor' as={Link} to={"/movies/latest"}>
 						<Card.Body>
 							<Card.Title>Latest Releases</Card.Title>
 							<Card.Text>Browse the latest film releases</Card.Text>
-							{/* <Link to={"/movies/latest"}>
-									<Button className='pink' variant='dark'>
-										Latest releases
-									</Button>
-								</Link> */}
 						</Card.Body>
 					</Card>
 					<Card
@@ -73,15 +76,12 @@ const HomePage = () => {
 							<Card.Text>
 								Browse the top rated movies of all time, as voted by our users
 							</Card.Text>
-							{/* <Link to={"/movies/top-listed"}>
-										<Button className='pink' variant='dark'>
-											Top Rated Movies
-										</Button>
-									</Link> */}
 						</Card.Body>
 					</Card>
 				</Container>
-
+				<div>
+					{storedValue && storedValue.map((movie) => <p>{movie.title}</p>)}
+				</div>
 				<Col>
 					<h2>
 						trending movies: {timeWindow === "day" ? "today" : "This week"}
